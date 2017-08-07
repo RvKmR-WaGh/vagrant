ccc110ad02529c7ea9e5a024f0589bc621602d66
Ravikumar Wagh <wagh1.ravi@gmail.com>
2017-08-08 01:29:30 +0530

    vagrant file to install tomcat

---
+++ install-tomcat7/provision.sh
@@ -0,0 +1,5 @@
+sudo apt-get -y install default-jdk
+wget http://apache.mivzakim.net/tomcat/tomcat-7/v7.0.79/bin/apache-tomcat-7.0.79.tar.gz
+sudo mv apache-tomcat-7.0.79.tar.gz /opt/
+sudo tar xvzf /opt/apache-tomcat-7.0.79.tar.gz
+
